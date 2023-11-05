c7d8f9069b3f15aee2b3f41633e270e2d4c2f7e8
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-11-05 16:07:17 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <ola/DmxBuffer.h>
+#include <ola/io/SelectServer.h>
+#include <ola/Logging.h>
+#include <ola/client/ClientWrapper.h>
+#include <ola/Callback.h>
+using std::cout;
+using std::endl;
+bool SendData(ola::client::OlaClientWrapper *wrapper) {
+ static unsigned int universe = 1;
+ static unsigned int i = 0;
+ ola::DmxBuffer buffer;
+ buffer.Blackout();
+ buffer.SetChannel(0, i);
+ wrapper->GetClient()->SendDMX(universe, buffer, ola::client::SendDMXArgs());
+ if (++i == 100) {
+ wrapper->GetSelectServer()->Terminate();
+ }
+ return true;
+}
+int main(int, char *[]) {
+ ola::InitLogging(ola::OLA_LOG_WARN, ola::OLA_LOG_STDERR);
+ ola::client::OlaClientWrapper wrapper;
+ if (!wrapper.Setup()) {
+ std::cerr << "Setup failed" << endl;
+ exit(1);
+ }
+ // Create a timeout and register it with the SelectServer
+ ola::io::SelectServer *ss = wrapper.GetSelectServer();
+ ss->RegisterRepeatingTimeout(25, ola::NewCallback(&SendData, &wrapper));
+ // Start the main loop
+ ss->Run();
+}
